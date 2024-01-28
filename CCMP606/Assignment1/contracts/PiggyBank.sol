3302c7de71476c876140b2f4de9b9894cac9273e
Joseph Nguyen <joseph.nguyen@geniebook.com>
2024-01-28 15:52:43 -0600

    can deposit and empty piggybank

--- CCMP606/Assignment1/contracts/PiggyBank.sol
+++ CCMP606/Assignment1/contracts/PiggyBank.sol
@@ -11,7 +11,7 @@ contract PiggyBank {
 
     address public immutable owner;
     uint public savingsGoal;
-    mapping(address=>uint) public deposits;
+    mapping(address=>uint256) public deposits;
     
     // Set any other variables you need
 
@@ -24,7 +24,7 @@ contract PiggyBank {
     }
     
     // Create an event to emit once you reach the savings goal
-    event SavingsGoalReached(address indexed depositor, uint amount);
+    event SavingsGoalReached(uint amount);
 
     // create modifier onlyOwner
     modifier onlyOwner() {
@@ -36,42 +36,39 @@ contract PiggyBank {
     // Function to receive ETH, called depositToTheBank
     // -- Function should log who sent the ETH 
     // -- Function should check balance to know if you've reached savings goal and emit the event if you have. 
-    receive() external payable {
-        depositToTheBank();
-    }
-
     function depositToTheBank() public payable {
-        require(msg.value > 0, "Deposit amount must be greater than 0");
-        
-        // Log who sent the ETH
-        deposits[msg.sender] += msg.value;
+    require(msg.value > 0, "Deposit amount must be greater than 0");
 
-        // Check if savings goal is reached and emit the event
-        if (address(this).balance >= savingsGoal) {
-            emit SavingsGoalReached(msg.sender, address(this).balance);
-        }
+    // Log who sent the ETH
+    deposits[msg.sender] += msg.value;
+
+    // Check if savings goal is reached and emit the event
+    if (deposits[msg.sender] >= savingsGoal) {
+        emit SavingsGoalReached(address(this).balance);
     }
+}
 
 
     // Function to return the balance of the contract, called getBalance
     // -- Note: you will need to use address(this).balance which returns the balance in Wei.
     // -- 1 Eth = 1 * 10**18 Wei
     function getBalance() public view returns (uint) {
-        return address(this).balance;
-    }
-
-    // Function to look up how much any depositor has deposited, called getDepositsValue
-    function getDepositsValue(address depositor) public view returns (uint) {
-        return deposits[depositor];
+        uint balance = address(this).balance * 10**18;
+        return balance;// deposits[msg.sender];
     }
 
 
     // Function to withdraw (send) ETH, called emptyTheBank
     // -- Only the owner of the contract can withdraw the ETH from the contract
    function emptyTheBank() public onlyOwner {
-        payable(owner).transfer(address(this).balance);
+        uint depositorBalance = deposits[msg.sender];
+        require(depositorBalance > 0, "No deposits to withdraw");
+        
+        // Transfer ETH to the owner
+        payable(owner).transfer(depositorBalance);
+
+        // Reset depositor's balance to 0 after withdrawal
+        deposits[msg.sender] = 0;
     }
-
-
 
 }
